3622f3c147c830a5dbfcb305cbcb921fbcb8619e
Earsouza <eduardo.ar2001@aluno.ifsc.edu.br>
2022-09-05 20:27:58 -0300

    Prova1

---
+++ src/ifsc/ProvaBMain.java
@@ -0,0 +1,29 @@
+package ifsc;
+
+import java.util.Scanner;
+
+public class ProvaBMain {
+
+	public static void main(String[] args) {
+		
+		Integer [] vetor = new Integer[6];
+		
+		Scanner ler = new Scanner(System.in.toString());
+
+		for(int i=0; i<vetor.length; i++) {
+			
+			Integer
+			
+			System.out.printf("Informe o %dº valor:\n", i+1);
+			Integer[i] = ler.nextLine();
+			
+		}
+		for(int i=0; i<vetor.length; i++) {
+			system.
+		}
+		
+		
+		
+	}
+
+	}
